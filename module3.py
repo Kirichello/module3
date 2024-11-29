b7dae83584ddbe8864d9157748a07d53ef0c2422
Kirichello <Kirill2811@gmail.com>
2024-11-29 21:08:18 +0300

    change color

---
+++ module3.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+def change_color():
+    match var.get():
+        case 0:
+            button['bg'] = 'red'
+        case 1:
+            button['bg'] = 'green'
+        case 2:
+            button['bg'] = 'black'
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry("250x250")
+    var = tk.IntVar()
+    var.set(0)
+    tk.Radiobutton(text='red', variable=var, value=0).pack()
+    tk.Radiobutton(text='green', variable=var, value=1).pack()
+    tk.Radiobutton(text='blue', variable=var, value=2).pack()
+    button = tk.Button(text='Изменить', command=change_color, width=10, height=5)
+    button.pack()
+    root.mainloop()
